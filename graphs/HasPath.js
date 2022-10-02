3362b68b39e328090a5915f8169815db3f58ab76
Mohamed Muzamil <mohd.muzamil.08@gmail.com>
2022-10-02 20:15:05 -0300

    Create HasPath.js

---
+++ graphs/HasPath.js
@@ -0,0 +1,21 @@
+// BFS iterative implementation that returns true if node is found in the graph
+
+const hasPath = (graph, src, dst) => {
+/*
+graph: adjacency list representing the graph
+src: source node
+dst: destination node
+*/
+  const queue = [ src ];
+  
+  while (queue.length > 0) {
+    const current = queue.shift();
+    
+    if (current === dst) return true;
+    
+    for (let neighbor of graph[src]) {
+      queue.push(neighbour); 
+    }
+  }
+  return false;
+};
