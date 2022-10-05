f38a0e4d0dc5875964a710f9c39b233e4ee2b09c
mohd-muzamil <mohd.muzamil.08@gmail.com>
2022-10-05 14:51:49 -0300

    update

--- others/BalancedSubstrings.py
+++ others/BalancedSubstrings.py
@@ -3,8 +3,7 @@ Method to return the number of ways in which a string can be split such that bot
 of opening and closing backets i.e. '(', ')', '[', ']'
 '?' can be used to replace as any of the bracket.
 '''
-
-form collections import Copy
+from collections import Counter
 from copy import copy
 
 def check(count):
@@ -16,22 +15,24 @@ def check(count):
     count['('] = max(count['('] , count[')'])
     count[')'] = max(count['('] , count[')'])
     
- if not(squar) and count['?'] >= abs(count['['] - count[']']):
-    count['?'] -= abs(count['['] - count[']'])
-    count['('] = max(count['['] , count[']'])
-    count[')'] = max(count['['] , count[']'])
- 
-return (count['('] == count[')']) and (count['['] == count[']']) and (count['?']%2 == 0)
+    if not(squar) and count['?'] >= abs(count['['] - count[']']):
+        count['?'] -= abs(count['['] - count[']'])
+        count['('] = max(count['['] , count[']'])
+        count[')'] = max(count['['] , count[']'])
+    
+    return (count['('] == count[')']) and (count['['] == count[']']) and (count['?']%2 == 0)
 
 
 def fillMissingBrackets(s):
   left_count = Counter()
   right_count = Counter(s)
   ret = 0
+  
   for i in s[:-1]:
     left_count[i] += 1
-    right_count[i] 1= 1
+    right_count[i] += 1
     if check(copy(left_count)) and check(copy(right_count)):
-      ret += 1
-   return ret
+        ret += 1
+  
+  return ret  
   
